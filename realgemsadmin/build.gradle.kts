c458110cd869eea9da74d2ca8482a8f4e2aaae8c
shaikhmoin28 <s.kmoin8425@gmail.com>
2024-07-03 19:29:40 +0530

    Home screen ,Categories,Product,All Gems Catgories Done

--- realgemsadmin/build.gradle.kts
+++ realgemsadmin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -41,7 +41,9 @@ buildFeatures{
 dependencies {
     implementation ("com.google.firebase:firebase-firestore-ktx:25.0.0")
     implementation ("com.airbnb.android:lottie:4.2.0")
+
     implementation ("com.squareup.picasso:picasso:2.71828")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- realgemsadmin/src/main/java/com/trinitydevelopers/realgemsadmin/bottomfragments/ExploreFragment.kt
+++ realgemsadmin/src/main/java/com/trinitydevelopers/realgemsadmin/bottomfragments/ExploreFragment.kt
@@ -6,20 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.trinitydevelopers.realgemsadmin.R
+import com.trinitydevelopers.realgemsadmin.adapter.CategoriesAdapter
 import com.trinitydevelopers.realgemsadmin.adapter.GemsAdapter
+import com.trinitydevelopers.realgemsadmin.adapter.ProductAdapter
 import com.trinitydevelopers.realgemsadmin.databinding.FragmentExploreBinding
-import com.trinitydevelopers.realgemsadmin.fragments.AddDropDownFragment
 import com.trinitydevelopers.realgemsadmin.fragments.SearchFragment
 import com.trinitydevelopers.realgemsadmin.pojos.Gems
 
 class ExploreFragment : Fragment() {
     private lateinit var binding:FragmentExploreBinding
     private lateinit var firestore: FirebaseFirestore
-    private lateinit var gemsAdapter: GemsAdapter
+    private lateinit var categoriesAdapter: CategoriesAdapter
+    private lateinit var pinnedProductAdapter: ProductAdapter
+
     private var gemsList = mutableListOf<Gems>()
 
     override fun onCreateView(
@@ -34,8 +37,14 @@ class ExploreFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         firestore = FirebaseFirestore.getInstance()
+
+        //categories
         setupRecyclerView()
         fetchGemsData()
+        //pinned products
+        setupPinnedProductRecyclerView()
+        fetchGemsPinnedProductData()
+
         binding.edtExploreSearch.setOnClickListener {
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
             transaction.replace(R.id.frame_container, SearchFragment())
@@ -43,10 +52,11 @@ class ExploreFragment : Fragment() {
             transaction.commit()
         }
     }
+    //categories
     private fun setupRecyclerView() {
-        gemsAdapter = GemsAdapter(requireContext(), gemsList)
-        binding.gemsRV.layoutManager = GridLayoutManager(context, 2)
-        binding.gemsRV.adapter = gemsAdapter
+        categoriesAdapter = CategoriesAdapter(requireContext(), gemsList)
+        binding.gemsRV.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+        binding.gemsRV.adapter = categoriesAdapter
     }
     private fun fetchGemsData() {
         firestore.collection("gems")
@@ -57,7 +67,30 @@ class ExploreFragment : Fragment() {
                     val gem = document.toObject(Gems::class.java)
                     gemsList.add(gem)
                 }
-                gemsAdapter.notifyDataSetChanged()
+                categoriesAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.w("ExploreFragment", "Error getting documents: ", exception)
+            }
+    }
+
+    //pinned products
+    private fun setupPinnedProductRecyclerView() {
+        pinnedProductAdapter = ProductAdapter(requireContext(), gemsList)
+        binding.pinnedProductRV.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+        binding.pinnedProductRV.adapter = pinnedProductAdapter
+    }
+
+    private fun fetchGemsPinnedProductData() {
+        firestore.collection("gems")
+            .get()
+            .addOnSuccessListener { documents ->
+                gemsList.clear()
+                for (document in documents) {
+                    val gem = document.toObject(Gems::class.java)
+                    gemsList.add(gem)
+                }
+                pinnedProductAdapter.notifyDataSetChanged()
             }
             .addOnFailureListener { exception ->
                 Log.w("ExploreFragment", "Error getting documents: ", exception)
